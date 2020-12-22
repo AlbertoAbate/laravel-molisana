df261e28e5daa6b21277adf5042c37f58acfd5a7
Alberto <alberto.abate87@gmail.com>
2020-12-22 17:42:58 +0100

    card in tipologie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,60 @@
 
 @section('content')
 
-<section>
+<section class="type-section">
+
+<h2>LE LUNGHE</h2>
+<div class="cards">
+    @foreach ($lunghe as $card)
+        <div class="card">
+            <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}">
+            <h3>{{ $card['titolo'] }}</h3>
+            <a href="">Vedi prodotto</a>
+        </div>
+            
+        @endforeach
+</div>
+
+</section>
+
+<section class="type-section">
+
+    <h2>LE CORTE</h2>
+    <div class="cards">
+        @foreach ($corte as $card)
+            <div class="card">
+                <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}">
+                <h3>{{ $card['titolo'] }}</h3>
+                <a href="">Vedi prodotto</a>
+            </div>
+                
+            @endforeach
+    </div>
+    
+    </section>
+
+    <section class="type-section">
+
+        <h2>LE CORTISSIME</h2>
+        <div class="cards">
+            @foreach ($cortissime as $card)
+                <div class="card">
+                    <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}">
+                    <h3>{{ $card['titolo'] }}</h3>
+                    <a href="">Vedi prodotto</a>
+                </div>
+                    
+                @endforeach
+        </div>
+        
+        </section>
+
+
+
+
+
+
+{{-- <section>
     <div class="cards">
         @foreach ($cards as $card)
         <div class="card">
@@ -13,6 +66,6 @@
             
         @endforeach
     </div>
-</section>
+</section> --}}
 
 @endsection
